f17e125c85f7b169a8647daf94ec999bd36d1eb3
Derose207 <127214511+Derose207@users.noreply.github.com>
2023-05-10 11:22:35 -0400

    afficher photo friend

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,26 @@ namespace ChatManager.Controllers
     public class ChatController : Controller
     {
         // GET: Chat
+        
         public ActionResult Index()
         {
+            //get id of the current user in the session
+           
             return View();
+            
         }
+        public ActionResult GetFriendList(bool forceRefresh = false)
+        {
+            if (forceRefresh || DB.Friendships.HasChanged)
+            {
+                //Send A list of all User tat is not the user in the sessions and is verified
+                List<User> listUser = DB.Friendships.GetFriends(OnlineUsers.GetSessionUser());
+
+
+                return PartialView(listUser);
+            }
+            return null;
+        }
+
     }
 }
